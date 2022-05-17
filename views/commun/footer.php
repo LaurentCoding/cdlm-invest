9b274a8bbb1578d8e3db590e4d5dfc2954d8947a
Tazlocoding <tazlo2728@gmail.com>
2022-05-17 17:51:40 +0200

    mise en place du footer

--- views/commun/footer.php
+++ views/commun/footer.php
@@ -1,8 +1,17 @@
-<footer class="d_flex  footer">
-    <div class="d_flex justify_content_center align_items_center">
-        <p>CDLM@Invest</p>
+<footer class=" footer">
+    <div class="d_flex justify_content_around  p_30">
+        <div>
+        <p class="footer_logo">CDLM@Invest</p>
+        <a  href="#">Mentions légales</a> | <a href="#">Politique de confidentialité</a> | <a href="#">Plan du site</a>
+        </div>
+        <div class="newsletter">
+            <a href="#">Abonnez-vous <br/>à notre Newsletter</a>
+        </div>
+        <div >
         <p>Tous droits réservés.</p>
         <span class="mr_5">FINTECH</span> <i class="fa-solid fa-copyright mr_5"></i><span>2022</span>
+        </div>
+       
     </div>
 </footer>
 
--- views/utilisateur/inscription.view.php
+++ views/utilisateur/inscription.view.php
@@ -1,41 +1,56 @@
 <!-- affichage formulaire -->
-<form method="POST" action="goInscription" class="d_flex align_items_center justify_content_center flex_column">
+
+<h1 class="inscription_titre">Créer un compte</h1>
+<form class="inscription_form mt_20" method="POST" action="goInscription" class="d_flex align_items_center justify_content_center flex_column">
     <table>
         <tr>
-            <td class="text_end">Nom :*</td>
+            <td class="form_name text_start mt_10">Nom :*</td>
+        </tr>
+        <tr>
             <td><input type="text" name="nom" id="nom" value="<?= $nom ?? "" ?>"></td>
         </tr>
         <tr>
             <td colspan="2" class="text_center"><span id="error_nom"></span></td>
         </tr>
         <tr>
-            <td class="text_end">Prenom :*</td>
+            <td class="form_name text_start mt_10">Prenom :*</td>
+        </tr>
+        <tr>
             <td><input type="text" name="prenom" id="prenom" value="<?= $prenom ?? "" ?>"></td>
         </tr>
         <tr>
             <td colspan="2" class="text_center"><span id="error_prenom"></span></td>
         </tr>
         <tr>
-            <td class="text_end">Email :*</td>
+            <td class="form_name text_start mt_10">Email :*</td>
+        </tr>
+        <tr>
             <td><input type="email" name="email" id="email" value="<?= $email ?? "" ?>"></td>
         </tr>
         <tr>
             <td colspan="2" class="text_center"><span id="error_email"></span></td>
         </tr>
         <tr>
-            <td class="text_end">Mot de passe :*</td>
+            <td class="form_name text_start mt_10">Mot de passe :*</td>
+        </tr>
+        <tr>
             <td><input type="password" name="password1" id="password1"></td>
         </tr>
         <tr>
-            <td class="text_end">Confirmation mot de passe :*</td>
+            <td class="form_name text_start mt_10">Confirmation mot de passe :*</td>
+        </tr>
+        <tr>
             <td><input type="password" name="password2" id="password2"></td>
         </tr>
         <tr>
             <td colspan="2" class="text_center"><span id="error_password"></span></td>
         </tr>
     </table>
-    <button type="submit">S'inscrire</button>
+    <div>
+        <button class="btn btn_valid mt_20" type="submit">S'inscrire</button>
+    </div>
 </form>
+
 <?php if (isset($_SESSION['inscription'])) { ?>
     <div>Votre Compte à bien été enregistré. <a href="<?= URL ?>connexion">Se connecter</a></div>
 
