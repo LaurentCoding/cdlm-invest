bcc44cd74f33a353c0512fcd891f78202085dcbc
Mathieu <mathieu.busse@live.fr>
2022-05-28 14:24:00 +0200

    update

--- views/utilisateur/projet.view.php
+++ views/utilisateur/projet.view.php
@@ -8,41 +8,48 @@
 
 <?php if (!empty($projets)) : ?>
     <div class="card_container text_center w_100 d_flex justify_content_center">
+
+
+
         <?php foreach ($projets as $projet) : ?>
 
             <div class="card card_projet w_30 br_10 p_15 m_10 d_flex flex_column ">
-
+                <i class="fa-solid fa-pencil btn_update_card"></i>
                 <!-- affichage nom projet -->
                 <div class="d_flex justify_content_around m_10">
-                <h3 class="card_title"><?= $projet->getNom_projet() ?></h3>
+                    <h3 class="card_title"><?= $projet->getNom_projet() ?></h3>
 
-                <!-- affichage modification non projet -->
-                <form  class="d_flex flex_column align align_items_center " action="<?= URL ?>user/projet/update/nom_projet/<?= $projet->getId() ?>" method="POST">
-                    <input type="text" name="nomProjet" id="nomProjet" value="<?= $projet->getNom_projet() ?>">
-                    <button class="card_projet-btn btn mt_10" type="submit">Modifier</button>
-                </form>
+                    <!-- affichage modification non projet -->
+                    <form class="form_nom_projet d_flex flex_column align align_items_center " action="<?= URL ?>user/projet/update/nom_projet/<?= $projet->getId() ?>" method="POST">
+                        <input type="text" name="nomProjet" id="nomProjet" value="<?= $projet->getNom_projet() ?>">
+                        <button class="card_projet-btn btn mt_10" type="submit">Modifier</button>
+                    </form>
                 </div>
                 <!-- affichage description -->
                 <div class="d_flex justify_content_around">
-                <h3 class="p_10"><?= $projet->getDescription_projet() ?></h3>
+                    <h3 class="p_10"><?= $projet->getDescription_projet() ?></h3>
 
-                <!-- afficher modifier description -->
-                <form class="d_flex flex_column align_items_center" action="<?= URL ?>user/projet/update/description_projet/<?= $projet->getId() ?>" method="POST">
-                    <textarea name="descriptionProjet" id="descriptionProjet" cols="20" rows="2" class="m_10 br_5 p_5"><?= $projet->getDescription_projet() ?></textarea>
-                    <button class="card_projet-btn btn  mt_10" type="submit">Modifier</button>
-                </form>
+                    <!-- afficher modifier description -->
+                    <form class=" form_description_projet d_flex flex_column align_items_center" action="<?= URL ?>user/projet/update/description_projet/<?= $projet->getId() ?>" method="POST">
+                        <textarea name="descriptionProjet" id="descriptionProjet" cols="20" rows="2" class="m_10 br_5 p_5"><?= $projet->getDescription_projet() ?></textarea>
+                        <button class="card_projet-btn btn  mt_10" type="submit">Modifier</button>
+                    </form>
                 </div>
 
                 <div class="d_flex justify_content_around ">
-                <div class="p_10">Investissement : <?= $projet->getInvestissement() ?> euros</div>
-                <div class="p_10">nombre d'année(s) : <?= $projet->getDuree() ?> <?php if($projet->getDuree() > 1){echo "ans";}else{echo "an";}?></div>
+                    <div class="p_10">Investissement : <?= $projet->getInvestissement() ?> euros</div>
+                    <div class="p_10">nombre d'année(s) : <?= $projet->getDuree() ?> <?php if ($projet->getDuree() > 1) {
+                                                                                            echo "ans";
+                                                                                        } else {
+                                                                                            echo "an";
+                                                                                        } ?></div>
                 </div>
 
                 <div class="m_20">
                     <a href="<?= URL ?>user/projet/afficher/<?= $projet->getId() ?>" class="btn btn_project m_5">Voir</a>
                     <a href="<?= URL ?>user/projet/delete/<?= $projet->getId() ?>" class="btn btn_warning m_5">Supprimer</a>
                 </div>
-              
+
             </div>
 
 
@@ -56,3 +63,25 @@
 <?php } else { ?>
     <a href="<?= URL ?>user/projet/new" class="card_newProject btn mt_20">Crée un nouveau projet</a>
 <?php } ?>
+
+
+
+<script>
+    const form_nom_projet = document.querySelectorAll('.form_nom_projet');
+    const form_description_projet = document.querySelectorAll('.form_description_projet');
+    const btn_form_card = document.querySelectorAll(".btn_update_card");
+    let i_event = 0
+
+    for (i = 0; i < form_nom_projet.length; i++) {
+        form_nom_projet[i].style.display = "none"
+    }
+    for (i = 0; i < form_description_projet.length; i++) {
+        form_description_projet[i].style.display = "none"
+    }
+
+    for (i = 0; i < btn_form_card.length; i++) {
+        i_event = i
+        btn_form_card[i].addEventListener('click', (event) => { console.log(event)})
+    }
+
+</script>
