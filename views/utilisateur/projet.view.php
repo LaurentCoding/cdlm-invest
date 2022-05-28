fc8c73a378383272171d2579d601993414d8c43c
Mathieu <mathieu.busse@live.fr>
2022-05-28 22:06:29 +0200

    js pour btn modif projet

--- views/utilisateur/projet.view.php
+++ views/utilisateur/projet.view.php
@@ -4,17 +4,14 @@
         <a href="#" class="btn">Débloquer la limite</a>
     <?php endif ?>
 </div>
-
-
+<?php $card = 0 ?>
 <?php if (!empty($projets)) : ?>
     <div class="card_container text_center w_100 d_flex justify_content_center">
 
-
-
+        <!-- card -->
         <?php foreach ($projets as $projet) : ?>
-
             <div class="card card_projet w_30 br_10 p_15 m_10 d_flex flex_column ">
-                <i class="fa-solid fa-pencil btn_update_card"></i>
+                <i id="<?= $card ?>" class="fa-solid fa-pencil btn_update_card"></i>
                 <!-- affichage nom projet -->
                 <div class="d_flex justify_content_around m_10">
                     <h3 class="card_title"><?= $projet->getNom_projet() ?></h3>
@@ -27,10 +24,10 @@
                 </div>
                 <!-- affichage description -->
                 <div class="d_flex justify_content_around">
-                    <h3 class="p_10"><?= $projet->getDescription_projet() ?></h3>
+                    <h3 class="p_10 card_description"><?= $projet->getDescription_projet() ?></h3>
 
                     <!-- afficher modifier description -->
-                    <form class=" form_description_projet d_flex flex_column align_items_center" action="<?= URL ?>user/projet/update/description_projet/<?= $projet->getId() ?>" method="POST">
+                    <form class=" form_description_projet d_flex flex_column align_items_center justify_content_center" action="<?= URL ?>user/projet/update/description_projet/<?= $projet->getId() ?>" method="POST">
                         <textarea name="descriptionProjet" id="descriptionProjet" cols="20" rows="2" class="m_10 br_5 p_5"><?= $projet->getDescription_projet() ?></textarea>
                         <button class="card_projet-btn btn  mt_10" type="submit">Modifier</button>
                     </form>
@@ -38,21 +35,20 @@
 
                 <div class="d_flex justify_content_around ">
                     <div class="p_10">Investissement : <?= $projet->getInvestissement() ?> euros</div>
-                    <div class="p_10">nombre d'année(s) : <?= $projet->getDuree() ?> <?php if ($projet->getDuree() > 1) {
-                                                                                            echo "ans";
-                                                                                        } else {
-                                                                                            echo "an";
-                                                                                        } ?></div>
+                    <div class="p_10">nombre d'année(s) : <?= $projet->getDuree() ?>
+                        <?php if ($projet->getDuree() > 1) {
+                            echo "ans";
+                        } else {
+                            echo "an";
+                        } ?></div>
                 </div>
 
                 <div class="m_20">
                     <a href="<?= URL ?>user/projet/afficher/<?= $projet->getId() ?>" class="btn btn_project m_5">Voir</a>
                     <a href="<?= URL ?>user/projet/delete/<?= $projet->getId() ?>" class="btn btn_warning m_5">Supprimer</a>
                 </div>
-
             </div>
-
-
+            <?php $card += 1 ?>
         <?php endforeach ?>
     </div>
 
@@ -66,23 +62,35 @@
 
 
 
+
 <script>
     const form_nom_projet = document.querySelectorAll('.form_nom_projet');
     const form_description_projet = document.querySelectorAll('.form_description_projet');
     const btn_form_card = document.querySelectorAll(".btn_update_card");
-    let i_event = 0
 
+    const titre_brut = document.querySelectorAll('.card_title');
+    const description_brut = document.querySelectorAll('.card_description');
+
+    // ------------------------
     for (i = 0; i < form_nom_projet.length; i++) {
         form_nom_projet[i].style.display = "none"
     }
     for (i = 0; i < form_description_projet.length; i++) {
         form_description_projet[i].style.display = "none"
     }
+    // ---------------------
 
     for (i = 0; i < btn_form_card.length; i++) {
-        i_event = i
-        btn_form_card[i].addEventListener('click', (event) => {
-            console.log(event)
-        })
+        btn_form_card[i].addEventListener('click',
+            (e) => {
+                i = e.target.id
+                form_nom_projet[i].style.display = "block"
+                form_description_projet[i].style.display = "block"
+                titre_brut[i].style.display = "none"
+                description_brut[i].style.display = "none"
+                btn_form_card[i].style.display = "none"
+            }
+        )
     }
+
 </script>
